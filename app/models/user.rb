0011de00726b444f0db2a440212aeccff286eadb
MKmacky <mkazuma529@icloud.com>
2021-03-12 17:16:39 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :encrypted_password, :password, :password_confirmation, 
-    format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
+
+  validates :encrypted_password, :password, :password_confirmation,
+            format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
   validates :nick_name,       presence: true
   validates :first_name,      presence: true,
-    format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
+                              format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }
   validates :last_name,       presence: true,
-    format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
+                              format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/ }
   validates :first_name_kana, presence: true,
-    format: { with: /\A[ァ-ヶー－]+\z/}
+                              format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :last_name_kana,  presence: true,
-    format: { with: /\A[ァ-ヶー－]+\z/}
+                              format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday,        presence: true
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    nick_name             {Faker::Name.name}
-    email                 {Faker::Internet.free_email}
+    nick_name             { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
     password              = Faker::Internet.password(min_length: 6)
-    password              {password}
-    password_confirmation {password}
-    first_name            {"山田"}
-    last_name             {"太郎"}
-    first_name_kana       {"ヤマダ"}
-    last_name_kana        {"タロウ"}
-    birthday              {"1996-05-29"}
+    password              { password }
+    password_confirmation { password }
+    first_name            { '山田' }
+    last_name             { '太郎' }
+    first_name_kana       { 'ヤマダ' }
+    last_name_kana        { 'タロウ' }
+    birthday              { '1996-05-29' }
   end
 end
