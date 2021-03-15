7e358f28a1cbde38df19d33db61bdf088a359f7d
MKmacky <mkazuma529@icloud.com>
2021-03-15 16:05:09 +0900

    itemモデル単体テストコード作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,12 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
-  
+
   with_options presence: true do
     validates :item_name
     validates :item_info
-    validates :item_price
+    validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :item_price, format: { with: /\A[0-9]+\z/ }
     validates :user
     with_options numericality: { other_than: 1 } do
       validates :category_id
