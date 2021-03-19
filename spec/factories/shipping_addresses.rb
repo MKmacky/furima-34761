ed36f936c8d528241c519915dafd3448c698cd6c
MKmacky <mkazuma529@icloud.com>
2021-03-19 13:35:20 +0900

    単体テストコード記述

--- spec/factories/shipping_addresses.rb
+++ spec/factories/shipping_addresses.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
   factory :shipping_address do
+    postal_code   {123-1234}
+    prefecture_id {6}
+    city          {city1}
+    addresses     {234}
+    building      {23}
+    phone_number  {09076543212}
+    association :user
+    association :item
+    association :buy_item
   end
 end
--- spec/models/shipping_address_spec.rb
+++ spec/models/shipping_address_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe ShippingAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe '商品購入機能' do
+    before do
+      @shipping_address = FactoryBot.build(:shipping_address)
+    end
+  end
+  
+  context '購入ができるとき' do
+    
+    it 'すべての値が正しく入力されていれば購入できる' do
+      expect(@donation_address).to be_valid
+    end
+
+    it 'buildingは空でも保存できること' do
+      @shipping_address.building = ''
+      expect(@donation_address).to be_valid
+    end
+
+  end
+
+  context '購入ができないとき' do
+
+    it 'postal_codeが空だと購入できない' do
+      @shipping_address.postal_code = ''
+      @shipping_address.valid?
+      expect(@shipping_address.errors.full_messages).to include("Postal code can't be blank")
+    end
+
+    it 'postal_codeが半角ハイフンを含まないと購入できない' do
+      @shipping_address.postal_code = '1234567'
+      @shipping_address.valid?
+      expect(@shipping_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+    end
+
+    it 'prefectureを選択していないと保存できないこと' do
+      @shipping_address.prefecture = 0
+      @shipping_address.valid?
+      expect(@shipping_address.errors.full_messages).to include("Prefecture can't be blank")
+    end
+    it 'cityが空では保存できないこと' do
+      @shipping_address.city = ''
+      @shipping_address.valid?
+      expect(@shipping_address.errors.full_messages).to include("City can't be blank")
+    end
+
+    it 'addressesが空では保存できないこと' do
+      @shipping_address.addresses = ''
+      @shipping_address.valid?
+      expect(@shipping_address.errors.full_messages).to include("Addresses can't be blank")
+    end
+    
+  end
+  
 end
