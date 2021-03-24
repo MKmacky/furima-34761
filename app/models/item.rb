89871117c5e950437488251c8b88b3d08fe92363
MKmacky <mkazuma529@icloud.com>
2021-03-24 11:53:40 +0900

    commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ class Item < ApplicationRecord
   belongs_to :scheduled_delivery
 
   with_options presence: true do
+    validates :image
     validates :item_name
     validates :item_info
     validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
