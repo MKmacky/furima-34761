b777ec471f9fc26a829531f8e023512f0252ec1e
MKmacky <mkazuma529@icloud.com>
2021-04-15 11:27:38 +0900

    機能修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,6 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one    :buy_item
   has_many_attached :images
-  has_many :item_tag_relations
-  has_many :tags, through: :item_tag_relations
   has_many :messages
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
